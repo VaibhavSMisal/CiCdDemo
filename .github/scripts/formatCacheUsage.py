e365ceaf044372833a11228707b374351b7370f7
VaibhavSMisal <133648683+VaibhavSMisal@users.noreply.github.com>
2023-12-26 17:46:33 +0530

    Update formatCacheUsage.py

--- .github/scripts/formatCacheUsage.py
+++ .github/scripts/formatCacheUsage.py
@@ -13,6 +13,8 @@ def generate_cache_usage_html():
             key = key_value_pair[0].split(":")[-1].strip()
             value = key_value_pair[1].split()[0].strip()
 
+            print(f"{key} {value}")
+
             html += f"<tr><td>{key}</td><td>{value}</td></tr>"
     
     html += "</table></details></body></html>"
