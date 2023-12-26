609faf08078dd875d68ef2b4a91d817d1173562b
VaibhavSMisal <133648683+VaibhavSMisal@users.noreply.github.com>
2023-12-26 16:28:06 +0530

    Modified formatCacheUsage.py

--- .github/scripts/formatCacheUsage.py
+++ .github/scripts/formatCacheUsage.py
@@ -1,20 +1,18 @@
 import sys
 def generate_cache_usage_html():
     html = "<html>"
-    html += "<body><h1>Bidder Token Generation Time</h1>"
+    html += "<body><h1>Cache Usage</h1><details><summary><h3 style=\"display:inline;\">Cache usage based on each action</h3></summary>"
     html += "<table>"
-    html += f"<tr><th>App Startup Mode</th><th>Valid Bidder Token in Cache</th><th>Generation time (90th percentile)</th></tr>"
+    html += f"<tr><th>Action in test-app</th><th>Cache usage(in Kb)</th></tr>"
 
     for i in range(0, len(table_list), 3):
         html += f"<tr><td>{table_list[i]}</td>" \
                 f"<td>{table_list[i+1]}</td>" \
                 f"<td>{format_time(table_list[i+2])}</td></tr>"
 
-    html += "</table>"
-    html += "<br><i>*refer all the App Startup modes <a href='https://developer.android.com/topic/performance/vitals/launch-time#startup-state'>here</a>.</i>"
-    html += "</body></html>"
+    html += "</table></details></body></html>"
 
-    with open("token_gen_time_report.html", "w") as file:
+    with open("cache_usage_report.html", "w") as file:
         file.write(html)
 
 def format_time(time):
