3536431fbbdfe742403d7674b524ef22eb6b9fcf
VaibhavSMisal <133648683+VaibhavSMisal@users.noreply.github.com>
2023-12-26 19:44:41 +0530

    Update formatCacheUsage.py

--- .github/scripts/formatCacheUsage.py
+++ .github/scripts/formatCacheUsage.py
@@ -26,7 +26,7 @@ def generate_cache_usage_html():
         file.write(html)
 
 logFile = open(sys.argv[1], "r")
-table_data = logFile.read()
+table_data = str(logFile.read())
 logFile.close()
 print(f"Table data = {table_data}")
 table_list = table_data.split("\n")
