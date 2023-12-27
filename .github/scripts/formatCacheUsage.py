3aa67b9f75738b02e80be5a75b2444da09729d3a
VaibhavSMisal <133648683+VaibhavSMisal@users.noreply.github.com>
2023-12-27 14:56:06 +0530

    Updated script for multi-table view

--- .github/scripts/formatCacheUsage.py
+++ .github/scripts/formatCacheUsage.py
@@ -1,20 +1,24 @@
 import sys
 def generate_cache_usage_html():
-    html = "<html>"
-    html += "<body><h1>Cache Usage</h1><details><summary><h3 style=\"display:inline;\">Cache usage based on each action</h3></summary>"
-    html += "<table>"
-    html += f"<tr><th>Action in test-app</th><th>Cache usage(in Kb)</th></tr>"
-
+    html += "<body><h1>Performance Profiling</h1><details><summary><h2 style=\"display:inline;\">Cache Memory Usage</h2></summary>"
+    current_module = None
+    
     for line in table_list:
-        print(f"Line: {line}")
         if "CacheSize:" in line:
+            # Extract module name from the line
+            module = line.split("#")[0].split(":")[-1].strip()
+            # If the module changes, print it and the header
+            if current_module != module:
+                if current_module != None:
+                    html += "</table></details>"
+                current_module = module
+                html += f"<details><summary><h3 style=\"display:inline;\">{current_module}</h3></summary>"
+                html += "<table><tr><th>Action in test-app</th><th>Cache usage</th></tr>"
             # Split the line by "=" to separate key and value
             key_value_pair = line.split("=")
             # Extract key and value
             key = key_value_pair[0].split(":")[-1].strip()
             value = key_value_pair[1].split()[0].strip()
-
-            print(f"Item: {key} {value}")
 
             html += f"<tr><td>{key}</td><td>{value}</td></tr>"
     
@@ -28,9 +32,7 @@ def generate_cache_usage_html():
 logFile = open(sys.argv[1], "r")
 table_data = logFile.read().strip()
 logFile.close()
-print(f"Table data = {table_data}")
 table_list = table_data.split('\n')
-print(f"List:\n{table_list}")
 
 generate_cache_usage_html()
 
