3aeae865a6b1eb346285750d6c99ce879e5c0618
VaibhavSMisal <133648683+VaibhavSMisal@users.noreply.github.com>
2024-01-03 18:22:15 +0530

    Removed all style tags

--- .github/scripts/formatCacheUsage.py
+++ .github/scripts/formatCacheUsage.py
@@ -1,10 +1,10 @@
 import sys
 def generate_cache_usage_html():
     html = "<html>"
-    html += "<body><h1 style='padding:0px; margin-top:0px; margin-bottom:0px;'>Performance Profiling</h1><details style='padding:0px; margin-top:0px; margin-bottom:0px;'><summary style='padding:0px; margin-top:0px; margin-bottom:0px;'>" \
-            "<h3 style='display:inline; padding:0px; margin-top:0px; margin-bottom:0px;'>Cache Memory Usage</h3></summary><ul>"
+    html += "<body><h1>Performance Profiling</h1><details><summary>" \
+            "<h3>Cache Memory Usage</h3></summary><ul>"
     current_module = table_list[0].split("#")[0].split(":")[-1]
-    html += f"<li><details><summary style='padding:0px; margin-top:0px; margin-bottom:0px;'><h4 style='display:inline; padding:0px; margin-top:0px; margin-bottom:0px;'>{current_module}</h4></summary>" \
+    html += f"<li><details><summary><h4>{current_module}</h4></summary>" \
             "<table><tr><th>Action in test-app</th><th>Cache usage</th></tr>"
     
     for line in table_list:
