23164c913e620a07704e5543b37997e1cd8ea24c
VaibhavSMisal <133648683+VaibhavSMisal@users.noreply.github.com>
2024-01-04 12:19:27 +0530

    Trying new dropdown using button tag, script and style

--- .github/scripts/formatCacheUsage.py
+++ .github/scripts/formatCacheUsage.py
@@ -1,10 +1,54 @@
 import sys
+import js2py
+
+js = """
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+""".replace("document.write", "return ")
+result = js2py.eval_js(js)
+
+style = """
+.collapsible {
+  background-color: #777;
+  color: white;
+  cursor: pointer;
+  padding: 18px;
+  width: 100%;
+  border: none;
+  text-align: left;
+  outline: none;
+  font-size: 15px;
+}
+
+.active, .collapsible:hover {
+  background-color: #555;
+}
+
+.content {
+  padding: 0 18px;
+  display: none;
+  overflow: hidden;
+  background-color: #f1f1f1;
+} """
+
 def generate_cache_usage_html():
-    html = "<html>"
-    html += "<body><h1>Performance Profiling</h1><details><summary>" \
-            "<h3>Cache Memory Usage</h3></summary><ul>"
+    html = f"<html><head><style>{style}</style></head>"
+    html += "<body><h1>Performance Profiling</h1>" \
+            "<button type='button' class='collapsible'>Cache Memory Usage</button><div class='content'>"
     current_module = table_list[0].split("#")[0].split(":")[-1]
-    html += f"<li><details><summary><h4>{current_module}</h4></summary>" \
+    html += f"<button type='button' class='collapsible'>{current_module}</button><div class='content'>" \
             "<table><tr><th>Action in test-app</th><th>Cache usage</th></tr>"
     
     for line in table_list:
@@ -17,7 +61,7 @@ def generate_cache_usage_html():
 
             html += f"<tr><td>{key}</td><td>{value[0:-1]} KB</td></tr>"
     
-    html += "</table></details></li></ul></details></body></html>"
+    html += f"</table></div></div><script>{result}</script></body></html>"
 
     print(f"HTML = {html}")
 
@@ -31,4 +75,3 @@ table_list = table_data.split('\n')
 print(f"Table lines = \n{table_list}")
 
 generate_cache_usage_html()
-
