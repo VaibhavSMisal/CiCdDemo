762a058207e37f62935d201134713f2a2de92806
VaibhavSMisal <133648683+VaibhavSMisal@users.noreply.github.com>
2024-01-15 15:06:16 +0530

    Create install-test-app.sh

---
+++ install-test-app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the class names
+output=$(find ./app/src/androidTest/java/com/example -type f -name '*.kt' | grep 'cicddemoapp' | awk -F'.kt|./app/src/androidTest/java/' '{print $2}' | tr '/' '.' | tr '\n' ',' | rev | cut -c 2- | rev)
+
+# Iterate over class names
+IFS=',' read -r -a class_names <<< "$output"
+for className in "${class_names[@]}"; do
+    adb shell am instrument -w -e class "$className" app.test/androidx.test.runner.AndroidJUnitRunner
+    adb shell pm clear com.example.cicddemoapp
+done
