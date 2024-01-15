7c018783e92f9fd4866218693b8dac0ba8662b26
VaibhavSMisal <133648683+VaibhavSMisal@users.noreply.github.com>
2024-01-15 16:26:25 +0530

    echo output and className

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -2,10 +2,13 @@
 
 # Get the class names
 output=$(find ./app/src/androidTest/java/com/example -type f -name '*.kt' | grep 'cicddemoapp' | awk -F'.kt|./app/src/androidTest/java/' '{print $2}' | tr '/' '.' | tr '\n' ',' | rev | cut -c 2- | rev)
-
+echo $output
 # Iterate over class names
 IFS=',' read -r -a class_names <<< "$output"
+echo $class_names
+
 for className in "${class_names[@]}"; do
+
     adb shell am instrument -w -e class "$className" app.test/androidx.test.runner.AndroidJUnitRunner
     adb shell pm clear com.example.cicddemoapp
 done
